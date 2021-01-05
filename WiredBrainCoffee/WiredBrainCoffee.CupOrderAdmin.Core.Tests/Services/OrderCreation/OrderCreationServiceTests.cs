eea4a262e82d3015ba38092527847601b642ae55
shaq105 <shaq105@hotmail.com>
2021-01-05 15:33:29 +0100

    m3:
    - video 2

--- WiredBrainCoffee/WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
+++ WiredBrainCoffee/WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WiredBrainCoffee.CupOrderAdmin.Core.Model;
+using WiredBrainCoffee.CupOrderAdmin.Core.Services.OrderCreation;
 
 namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
 {
@@ -7,9 +10,21 @@ namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
     public class OrderCreationServiceTests
     {
         [TestMethod]
-        public void ShouldStoreCreatedOrderInOrderCreationResult()
+        public async Task ShouldStoreCreatedOrderInOrderCreationResult()
         {
-            throw new NotImplementedException();
+            var orderCreationService = new OrderCreationService(null, null);
+            var numberOfOrderedCups = 1;
+            var customer = new Customer
+            {
+                Id = 99
+            };
+
+            var orderCreationResult = 
+            await orderCreationService.CreateOrderAsync(customer, numberOfOrderedCups);
+
+            Assert.AreEqual(OrderCreationResultCode.Success, orderCreationResult.ResultCode);
+            Assert.IsNotNull(orderCreationResult.CreatedOrder);
+            Assert.AreEqual(customer.Id, orderCreationResult.CreatedOrder.CustomerId);
         }
     }
 }
