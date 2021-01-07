e76f5b2a8bd2058be10dedda8e4ae4ee0ef953f2
shaq105 <shaq105@hotmail.com>
2021-01-07 13:04:25 +0100

    m4:
    video 6

--- WiredBrainCoffee/WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
+++ WiredBrainCoffee/WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTests.cs
@@ -104,8 +104,10 @@ namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
         [DataTestMethod]
         [DataRow(3, 5, CustomerMembership.Basic)]
         [DataRow(0, 4, CustomerMembership.Basic)]
+        [DataRow(0, 1, CustomerMembership.Basic)]
         [DataRow(8, 5, CustomerMembership.Premium)]
         [DataRow(5, 4, CustomerMembership.Premium)]
+        [DataRow(5, 1, CustomerMembership.Premium)]
         public void ShouldCalculateCorrectDiscountPercentage(
             double expectedDiscountInPercent,
             int numberOfOrderedCups, 
